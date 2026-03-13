331878fd196dcea891c9935a4f5960c7460b5432
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2026-03-13 18:10:27 +1100

    chore: initialise project

---
+++ StudySphere/StudySphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySphereApp.swift
+//  StudySphere
+//
+//  Created by Anh Nguyen on 13/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct StudySphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
