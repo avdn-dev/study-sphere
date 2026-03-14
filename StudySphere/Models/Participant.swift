33fced8a8b3d331ba27ba8a4205f94e8389c0b90
GenericUser8 <unwound.podium_0y@icloud.com>
2026-03-14 16:19:03 +1100

    Added peerIDData back to Participant

--- StudySphere/Models/Participant.swift
+++ StudySphere/Models/Participant.swift
@@ -2,6 +2,7 @@ import Foundation
 
 struct Participant: Codable, Equatable, Identifiable, Hashable, Sendable {
     let id: UUID
+    let peerIDData: Data
     var name: String
     var avatarSystemName: String = "person.circle.fill"
     var status: ParticipantStatus
